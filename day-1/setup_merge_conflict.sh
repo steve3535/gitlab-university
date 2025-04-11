857a2d3ef72dcaebeb818d5c33ea59b575f4d859
kwakou <kwakou.missoh@lxp.lu>
2025-04-11 21:56:30 +0200

    update

--- day-1/setup_merge_conflict.sh
+++ day-1/setup_merge_conflict.sh
@@ -8,19 +8,19 @@ cd mergesort
 git init
 
 # Add the base implementation
-\cp ../mergesort/base.py mergesort.py
+\cp ../mergesort-files/base.py mergesort.py
 git add mergesort.py
 git commit -m "Fake it till you make it - Faking mergesort using python .sort() method"
 
 # Create a new branch for the mergesort implementation
 git checkout -b "Mergesort-Impl"
-\cp ../mergesort/righty.py mergesort.py
+\cp ../mergesort-files/righty.py mergesort.py
 git add mergesort.py
 git commit -m "Mergesort implemented on feature branch"
 
 # Switch back to main branch
 git checkout main
-\cp ../mergesort/lefty.py mergesort.py
+\cp ../mergesort-files/lefty.py mergesort.py
 git add mergesort.py
 git commit -m "Mergesort implemented on main"
 
